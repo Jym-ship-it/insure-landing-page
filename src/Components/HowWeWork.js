df95c26eddc156d57807751f01033405ccd4d98f
Jym-ship-it <fjymcyruz@gmail.com>
2023-02-21 19:37:43 +0800

    Update HowWeWork.js

--- src/Components/HowWeWork.js
+++ src/Components/HowWeWork.js
@@ -6,15 +6,30 @@ import IntroLeftDT from '../Assets/Images/bg-pattern-intro-left-desktop.svg'
 export default function HowWeWork() {
     const style = {
         mainContainer : {
-            padding : "200px 180px",
+            padding : "240px 180px",
             backgroundImage : `url(${IntroLeftDT})`,
             backgroundRepeat : "no-repeat",
             backgroundPosition : "left -208px"
-        }
+        },
+        divider: {
+            width: "180px",
+            height: "1.5px",
+            backgroundColor: "hsl(270, 9%, 17%)",
+          },
+         workTitle: {
+            fontFamily: "DM Serif Display",
+            fontWeight: "500",
+            fontSize: "70px",
+            marginTop: "80px",
+            color: "hsl(270, 9%, 17%)",
+            width: "35vw",
+            lineHeight: "8vh",
+          },
     }
   return (
    <Box sx = {style.mainContainer}>
-    <Typography>TEst</Typography>
+    <Box sx={style.divider}></Box>
+    <Typography sx={style.workTitle}>We're different</Typography>
    </Box>
   )
 }
